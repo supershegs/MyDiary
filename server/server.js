afe7c5bf709bd318bebe8794904e91f1770877eb
Oluwasegun <segsamos@gmail.com>
2018-07-17 03:39:56 +0100

    setting up express and babel to transpire Es6 or higher codes in server.js to Es5

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+let app = express();
+const port = 3000;
+
+app.get('/', (req,res)=>{
+    res.status(200).send({"message": "API is running"});
+});
+
+app.listen(port, ()=>{
+    console.log("server is running on port", {port})
+});
