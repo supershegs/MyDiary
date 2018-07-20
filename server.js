71743c3c1eb85b11be252b3e231d0060e8a8a3bd
Oluwasegun <segsamos@gmail.com>
2018-07-20 18:57:39 +0100

    adding public and making some arrangement

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import path from 'path';
+import expressHBS from 'express-handlebars';
 import firstApi from './routes/ver_Api_1/index';
-import secondApi from './routes/ver_Api_2/index';
-
+// import secondApi from './routes/ver_Api_2/index';
+ 
 const app = express();
 const port = 3000;
 
+ 
+app.engine('handlebars', expressHBS({ defaultLayout: 'header' }));
+app.set('view engine', 'handlebars');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
 app.use('/api/v1', firstApi);
-app.use('/api/v2', secondApi);
+// app.use('/api/v2', secondApi);
+ 
 
 const server = app.listen(port, () => {
     console.log('server is running on port', port);
@@ -15,5 +25,4 @@ const server = app.listen(port, () => {
 export function closeSever() {
     server.close();
 }
-
 export default app;
