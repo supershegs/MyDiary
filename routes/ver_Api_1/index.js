a96812766c54a2f09d491c8bbb8eec2f16ae86ec
Oluwasegun <segsamos@gmail.com>
2018-07-20 13:21:51 +0100

    setting my version one, setting the handlebars to display html files

--- routes/ver_Api_1/index.js
+++ routes/ver_Api_1/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.status(200).send('version one ok');
 });
+router.get('/index', (req, res) => {
+    res.render('index');
+});
 router.get('/user', (req, res) => {
     res.send('version one user');
 });
