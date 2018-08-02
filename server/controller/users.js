f79284b69420673e3f9278b02ed2a2cb6a22efbc
Oluwasegun <segsamos@gmail.com>
2018-08-02 12:45:27 +0100

    change the login method by setting it into the controller with jwt setting

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,5 +1,20 @@
 import userModel from '../models/users';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import { Client } from 'pg';
+import jwt from 'jsonwebtoken';
+
+
+dotenv.config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+client.connect();
 
 const users = {
   createUser(request, response) {
@@ -14,15 +29,46 @@ const users = {
     response.status(201).json({ message: 'Successfully added' });
     return user;
   },
-  authUser(request, response, err) {
-    const auth = userModel.signin(request.body);
-    const nam = request.body.username;
-    const pas = request.body.password;
-    if (nam.length === 0 || pas.length === 0) {
-      return response.status(404).json({ error: 'please filled in the correct details' });
+  authUser(request, response) {
+    const user = {
+      username: request.body.username,
+      password: request.body.password,
+    };
+    if (user.username.length > 0 || user.password > 0) {
+      client.query(`SELECT * from users WHERE username = '${user.username}' `, (err, result) => {
+        if (err) {
+          response.status(404).json({ error: 'Authentication Failed', err });
+        } if (result) {
+          const seen = result.rows;
+          if (seen.length === 0 || !seen[0].password) {
+            response.status(404).json({ message: 'Not Found' });
+          } else if (seen.length > 0) {
+            bcrypt.compare(user.password, seen[0].password, (error, accept) => {
+              if (error) {
+                response.status(404).json({
+                  message: 'not added',
+                  status: error,
+                });
+              }
+              if (accept) {
+                const token = jwt.sign({
+                  username: seen[0].username,
+                  id: seen[0].id,
+                }, process.env.SECRET_JWT_KEY,
+                {
+                  expiresIn: '1h',
+                });
+                response.status(201).json({
+                  message: 'Authentication Passed',
+                  status: accept,
+                  token_key: token,
+                });
+              }
+            });
+          }
+        }
+      });
     }
-    response.status(200).json({ message: 'Authetication successful' });
-    return auth;
   },
 };
 
--- server/models/users.js
+++ server/models/users.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import uuidv4 from 'uuid/v4';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { throws } from 'assert';
 
 dotenv.config();
 const client = new Client({
@@ -40,7 +39,6 @@ class Usermodel {
               } else {
                 users = (result.rows);
                 return users;
-              // const token = jwt.sign({ response.ro, process.env.SECRET_KEY});
               }
             });
           }
@@ -48,36 +46,6 @@ class Usermodel {
           throw new Error('invalid input');
         }
       });
-  }
-
-  static signin(data) {
-    const user = {
-      username: data.username,
-      password: data.password,
-    };
-    if (data.username.length > 0 || data.password > 0) {
-      client.query(`SELECT * from users WHERE username = '${data.username}' `, (err, res) => {
-        if (err) {
-          throw new Error('not found');
-        } else {
-          const seen = res.rows;
-          console.log('5', seen[0].username);
-          if (seen.length === 0 || !seen[0].password) {
-            throw new Error('Authentication failed');
-          } else if (seen.length > 0) {
-            bcrypt.compare(data.password, seen[0].password)
-              .then((error, result) => {
-                if (error) {
-                  throw new Error('not an hashed password', error);
-                }
-                if (result) {
-                  console.log('successfully compared', result);
-                }
-              });
-          }
-        }
-      });
-    }
   }
 }
 export default Usermodel;
